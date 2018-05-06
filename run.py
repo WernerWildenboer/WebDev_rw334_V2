fecd4426d375358d77dad91c1af1428685afa67b
Marthinus Bosman <18955185@sun.ac.za>
2018-05-06 15:47:34 +0200

    one whole site redo coming up

--- run.py
+++ run.py
@@ -190,4 +190,4 @@ def question(question):
 
 port = int(os.environ.get('PORT', 5000))
 app.secret_key = os.urandom(24)
-app.run(host='0.0.0.0', port=port)
+app.run(host='0.0.0.0', port=port, debug=True)
