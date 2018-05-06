3ec645c99413be77efe12a730195e441f8be5940
Werner_Wildenboer <wwildenboer@gmail.com>
2018-05-06 11:53:23 +0200

    Revert "Dis nou n power play dude ♂"
    This reverts commit 92962cbee141d3066db0f51c3f6b41d13773483a.

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ from datetime import datetime
 import uuid
 from urllib.parse import urlparse, urlunparse
 
-##############################_Link to database_#################################
+#############################  Link to database  ################################
 
 authenticate("hobby-ogikmifabjnfgbkeflkddeal.dbs.graphenedb.com:24780", "cs334", "b.soC0X1oY5V3W.npSyseCrhtODLqgc")
 graph = Graph("https://hobby-ogikmifabjnfgbkeflkddeal.dbs.graphenedb.com:24780/db/data/", bolt = False)
