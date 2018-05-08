35bbb88bd2b02c879a1b9f0bfca834f2d635a26e
RPasch <pasch.k.r@gmail.com>
2018-05-08 19:07:42 +0200

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	
 	function myFunction() {
+		alert("myFunction");
     		var x = document.getElementById("Demo");
    		 if (x.className.indexOf("w3-show") == -1) {
        			 x.className += " w3-show";
