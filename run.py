9e352d6b51f75a86086ebbca8b5d171f8fc693a0
Werner L. Wildenboer <wwildenboer@gmail.com>
2018-05-08 19:41:13 +0200

    should work

--- run.py
+++ run.py
@@ -226,14 +226,8 @@ def show_topics():
 @app.route('/show_suggestions')
 def show_suggestions():
 	suggestions = []
-	query = '''MATCH a=((user_1:User {username:'{username}'})-[r1:FOLLOWS]->
-(user_2:User)-[r2:FOLLOWS]->(user_3:User))
-WHERE user_1.username<>user_3.username
-AND NOT (user_1)-[:FOLLOWS]->(user_3)
-MATCH (user_3)-[:WROTE]->()<-[upvotes:UPVOTE]-()
-RETURN user_3.username AS Username,COUNT(upvotes) AS Rank ORDER BY Rank DESC;'''
-    query = query.format(username=session['username'])
-	suggestions = graph.run(query)
+   
+
 	return render_template('show_suggestions.html', suggestions=suggestions)
 	
 @app.route('/user')
