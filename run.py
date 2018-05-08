d4d159ef0e3dfeaecc362aa9722ae188b08b4738
Werner L. Wildenboer <wwildenboer@gmail.com>
2018-05-09 01:53:07 +0200

    dddd

--- run.py
+++ run.py
@@ -175,7 +175,7 @@ def login():
 
 	return render_template('login.html', title="Login")
 
-#==============================================================================================================
+#================================================User(session['username']).answer_question(question, answer)
 @app.route('/change_password',methods=['GET', 'POST'])
 def change_password():
 	if request.method == 'POST':
@@ -185,16 +185,7 @@ def change_password():
 		if not User(username).verify_password(password_old):
 			flash('Invalid login.')
 		else:
-			session['username'] = username
-			user = User(session['username']).find()
-			password = password_new
-			query ='''MATCH (n:User)
-            WHERE n.username='{username}'
-            SET n.password = "{password_q}"'''
-			query = query.format(username=session['username'],password_q=password)
-			
-			change_password = graph.run(query)
-			return redirect(url_for('index'))
+
         
 	return render_template('change_password.html', title="Change Password")
 
