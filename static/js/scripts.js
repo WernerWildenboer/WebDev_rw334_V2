24d9b1021e12c75938c8360b7f0643ba58bfb5aa
Emile Lombard <>
2018-05-08 23:44:24 +0200

    toets

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -9,14 +9,6 @@ $(document).ready(function() {
 		}
 	
 	//show dropdown on home page.
-	$("#dropdownBtn").click(function() {
-		 var temp = this.id;
-		 if( !temp.hasClass("w3-show") ) {
-			temp.addClass("w3-show").removeClass("w3-hide");
-		 } else {
-			temp.addClass("w3-hide").removeClass("w3-show");
-		 }
-	});
 	
 	//Home Page AJAX functions
 	function replaceTopic() {
