ec0eb0bcc273c1cab25d0d00638e74c190525538
Werner L. Wildenboer <wwildenboer@gmail.com>
2018-05-09 09:16:53 +0200

    ☺

--- run.py
+++ run.py
@@ -253,8 +253,12 @@ def question(question):
 #=====================================*_ | Search | _START_*=====================================
 @app.route('/search')
 def search():
-    search_string = request.form['search_string_from_usersearch_string_from_user']
-	return render_template('search.html', title="Users", search_string=search_stringn)
+	search_string = request.form['search_string_from_usersearch_string_from_user']
+		query ='''MATCH (n:User)
+WHERE n.username =~ '.*{search_string}.*'
+RETURN n.username'''
+	list_usernames = query.format(search_string=search_string)
+	render_template('search.html', title="Users", list_usernames=list_usernames)
 #=====================================*_ | Search | _END_*=====================================
 @app.route('/show_topics')
 def show_topics():
