428c57987ad7d58dfdbe83c5db0ca3eaf93e152d
Werner L. Wildenboer <wwildenboer@gmail.com>
2018-05-09 02:16:59 +0200

    ONS IS IN BUSINESS , FUCK YES BOYS

--- run.py
+++ run.py
@@ -268,7 +268,7 @@ def show_suggestions():
 WHERE user_1.username<>user_3.username
 AND NOT (user_1)-[:FOLLOWS]->(user_3) AND user_1.username='{username}'
 MATCH (user_3)-[:WROTE]->()<-[upvotes:UPVOTE]-()
-RETURN user_3.username AS Username,COUNT(upvotes) AS Rank ORDER BY Rank DESC;'''
+RETURN user_3.username AS name,COUNT(upvotes) AS rank ORDER BY Rank DESC;'''
     query = query.format(username=session['username'])
     suggestions = graph.run(query)
     return render_template('show_suggestions.html', suggestions=suggestions)
