7f1074d2a4396eb29e9b13b20da70679df78ff9b
Werner L. Wildenboer <wwildenboer@gmail.com>
2018-05-09 12:19:55 +0200

    f

--- run.py
+++ run.py
@@ -280,7 +280,7 @@ CREATE (a)-[r:FOLLOWS]->(b)
 RETURN type(r)'''
 		query = query.format(user_1=session['username'],user_2=user_2)
 		follows = graph.run(query)
-		else:   
+		  
 		return redirect(url_for('index'))
 	return redirect(url_for('index'))
 #=====================================*_ | Follow | _END_*=====================================
